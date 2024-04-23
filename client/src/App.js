6c938063fd64ec2cc9cc6487430f2bb683edc323
Allison Driver <allisondriver04@gmail.com>
2024-04-22 19:02:39 -0500

    added looping functions to extract info

--- client/src/App.js
+++ client/src/App.js
@@ -46,9 +46,78 @@ function App() {
 
   },[]);
 
+
+//getting a list of tracks from the playlist
 const getNowPlaying = () => {
-  spotifyApi.getMyTopTracks().then((response) => {
-    console.log(response);
+  spotifyApi.getPlaylist("37i9dQZF1DX4dyzvuaRJ0n?si=967acf10164e49d4").then((response) => {
+    //listing out the tracks
+    var tracks = response.tracks.items;
+    console.log(tracks)
+
+    
+    
+    //creating an array to house the track id's
+    let trackIds = [];
+   
+    //looping through the tracks and pushing the track id info into the trackIds array
+    for(let i=0; i < tracks.length;i++){
+      //console.log(tracks[i].track.id)
+      var trackid=[tracks[i].track.id];
+      trackIds.push(trackid);
+    }
+    console.log("trackIds:" + trackIds);
+
+   
+    for(let i=0; i < tracks.length;i++){
+      var trackartist = tracks[i].track.artists
+      console.log(trackartist)
+      ///////PICK UP HERE/////////
+      //loop through trackartists to get artist name
+    }
+    
+   
+    
+//creating an array for audio features of each trackId 
+    let trackFeatures = [];
+//inputing the track id's into the audio features function to get features for each song id
+    trackIds.forEach(item => 
+    spotifyApi.getAudioFeaturesForTrack(item).then((response) => {
+      console.log(response)
+      trackFeatures.push(response)
+    }
+   
+  )
+  
+)
+
+console.log(trackFeatures)
+
+
+//creating an array of objects to house the specific track info and featuers that I want to save
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    
+    
+    
+    
+    
+    
+
+    //spotifyApi.getAudioFeaturesForTracks(response.tracks.items.)
    
     })
   }
